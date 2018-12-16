f6e93a09f2942ae8b90177ca4347e79c2ffa884d
david <david@imac-de-david.home>
2018-12-16 18:46:57 +0100

    gogogo

--- client/components/booking/form/BookingForm.vue
+++ client/components/booking/form/BookingForm.vue
@@ -4,7 +4,7 @@
         <p class="uppercase bg-moonrise mb-4 p-2 text-white text-sm">1. Booker information</p>
   <div class="flex flex-wrap -mx-3 mb-6">
     <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
-        <app-input v-model="booking_request.booker_handle" type="text">
+        <app-input v-model="booking_request.booker_orga_handle" type="text">
             Booker organisation name
         </app-input>
     </div>
@@ -96,20 +96,78 @@
     </div>
   </div>
   </template>
+   
+   <p class="uppercase bg-moonrise mb-4 p-2 text-white text-sm">2. Company Data for contract and invoice</p>
+      <div class="flex flex-wrap -mx-3 mb-6">
+    <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+        <app-input v-model="booking_request.company_name" type="text">
+            Company name
+        </app-input>
+    </div>
+    <div class="w-full md:w-1/2 px-3">
+         <app-input v-model="booking_request.company_website" type="text">
+            Company Website
+        </app-input>
+    </div>
+  </div>
+
+  <div class="flex flex-wrap -mx-3 mb-6">
+    <div class="w-full px-3">
+        <app-input v-model="booking_request.company_address" type="text">
+            Company / Promoter address (<span class="text-red text-xs">please include zip code</span>)
+        </app-input>
+    </div>
+  </div>
+
+  <div class="flex flex-wrap -mx-3 mb-6">
+    <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+        <app-input v-model="booking_request.company_contract_signatory" type="text">
+          Contract signatory (<span class="text-red text-xs">full name</span>)
+        </app-input>
+    </div>
+    <div class="w-full md:w-1/2 px-3">
+         <app-input v-model="booking_request.company_signatory_position" type="text">
+            Position in organisation
+        </app-input>
+    </div>
+  </div>
+    <div class="flex flex-wrap -mx-3 mb-6">
+    <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+        <app-input v-model="booking_request.company_email" type="email">
+          Company email
+        </app-input>
+    </div>
+  </div>
+
+  <div class="flex flex-wrap -mx-3 mb-6">
+    <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+        <app-input v-model="booking_request.company_office_phone" type="text">
+          Office phone (<span class="text-red text-xs">inc international code</span>)
+        </app-input>
+    </div>
+    <div class="w-full md:w-1/2 px-3">
+         <app-input v-model="booking_request.company_mobile_phone" type="text">
+            Mobile (<span class="text-red text-xs">inc international code</span>)
+        </app-input>
+    </div>
+  </div>
+    <div class="flex flex-wrap -mx-3 mb-6">
+    <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+        <app-input v-model="booking_request.company_vat" type="text">
+          Company VAT Number
+        </app-input>
+    </div>
+  </div>
+
+   
 </form>
 </template>
 
 
-// Company Data for contract and invoice 
-// Company Name:
-//Company/Promoter address (please include zip code):
-//VAT number: 	
-//Website:
-//Contract signatory full name:
+// full name:
 // Position in Organisation
 //Email address:
-//Office telephone inc full international dial code:			
-// Mobile telephone inc full international dial code:
+
 
 // Event date: (DD/MM/YY)
 // Event name:
@@ -138,7 +196,6 @@
 // Artists booked in this venue during last 12 months (supply names and exact dates):  Artists booked by the promoter in different venues during the last 12 months (supply names and exact dates):	 		
 // References (please give names and email addresses of agencies/artists who would give promoter references):	
 
-
 <script>
 import AppToggleInput from '~/components/ui/AppToggleInput.vue'
 import AppInput from '~/components/ui/AppInput.vue'
@@ -151,7 +208,7 @@ export default {
     data() {
         return {
             booking_request: {
-                booker_handle: "",
+                booker_orga_handle: "",
                 booker_website: "",
                 booker_address: "",
                 booker_contact_person: "",
@@ -169,8 +226,15 @@ export default {
                     full_name: "",
                     email: "",
                     phone_number: ""
-                }
-
+                },
+                company_name: "",
+                company_email: "",
+                company_address: "",
+                company_office_phone: "",
+                company_mobile_phone: "",
+                company_vat: "",
+                company_contract_signatory: "",
+                company_signatory_position: ""
 
             }
         }
