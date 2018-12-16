2b55ed9ab11ed861f36b5ac6dcdac675c860ef32
Eloi CHOUVET <echouvet@e1r13p8.42.fr>
2018-12-16 19:03:53 +0100

    grid update

--- client/components/grid/ImageGrid.vue
+++ client/components/grid/ImageGrid.vue
@@ -1,64 +1,83 @@
 <template>
-    <div class="flex flex-wrap px-2"> 
+
+<!-- FOUR ROWS -->
+<!--     <div class="flex flex-wrap px-2"> 
          <div class="w-full sm:w-1/4 px-1">
-            <img v-for="(el, i) in images" v-if="i < row_count" :src="el.img1" :key="el.id" class="w-full"> 
+            <img v-for="(el, i) in images" v-if="i < row_count4" :src="el.img1" :key="el.id" class="w-full"> 
         </div>
         <div class="w-full sm:w-1/4 px-1">
-            <img v-for="(el, i) in images" v-if="i > row_count && i <= (row_count * 2)" :src="el.img1" :key="el.id" class="w-full"> 
+            <img v-for="(el, i) in images" v-if="i >= row_count4 && i < (row_count4 * 2)" :src="el.img1" :key="el.id" class="w-full"> 
         </div>
         <div class="w-full sm:w-1/4 px-1">
-           <img v-for="(el, i) in images" v-if="i > (row_count * 2) && i <= (row_count * 3)" :src="el.img1" :key="el.id" class="w-full"> 
+           <img v-for="(el, i) in images" v-if="i >= (row_count4 * 2) && i < (row_count4 * 3)" :src="el.img1" :key="el.id" class="w-full"> 
         </div>
         <div class="w-full sm:w-1/4 px-1">
-           <img v-for="(el, i) in images" v-if="i > (row_count * 3) && i <= (row_count * 4)" :src="el.img1" :key="el.id" class="w-full"> 
+           <img v-for="(el, i) in images" v-if="i >= (row_count4 * 3)" :src="el.img1" :key="el.id" class="w-full"> 
+        </div>
+    </div> -->
+
+<!-- THREE ROWS -->
+    <div class="flex flex-wrap px-2"> 
+         <div class="w-full sm:w-1/3 px-1">
+            <img v-for="(el, i) in images" v-if="i < row_count3" :src="el.img1" :key="el.id" class="w-full"> 
+        </div>
+        <div class="w-full sm:w-1/3 px-1">
+            <img v-for="(el, i) in images" v-if="i >= row_count3 && i < (row_count3 * 2)" :src="el.img1" :key="el.id" class="w-full"> 
+        </div>
+        <div class="w-full sm:w-1/3 px-1">
+           <img v-for="(el, i) in images" v-if="i >= (row_count3 * 2)" :src="el.img1" :key="el.id" class="w-full"> 
         </div>
     </div>
+
+<!-- TWO ROWS -->
+<!--     <div class="flex flex-wrap px-2"> 
+         <div class="w-full sm:w-1/2 px-1">
+            <img v-for="(el, i) in images" v-if="i < row_count2" :src="el.img1" :key="el.id" class="w-full"> 
+        </div>
+        <div class="w-full sm:w-1/2 px-1">
+            <img v-for="(el, i) in images" v-if="i >= row_count2" :src="el.img1" :key="el.id" class="w-full"> 
+        </div>
+    </div>
+ -->
+
+
 </template>
 
 <script>
 export default {
     data() {
         return {
-            count: 0,
-            row_count: 5,
+            row_count4: 0,
+            row_count3: 0,
+            row_count2: 0,
             images: {},
 
         }
-    },
+    },   
+
+    //   computed: {
+    //     imageCount() {
+    //       return Object.keys(this.images).length;
+    //     },
+    // },
     mounted () {
         this.getartists()
     },
     methods: {
-        getartists() {
+        getartists()
+        {
             this.$http.get('http://localhost:4000/artists').then(response => {
                 var data =  JSON.parse(response.data)
                 console.log(data[0].img1)
                 data.forEach(el => {
                     el.img1 = el.img1.replace("/__nuxt/assets/img/", "/img/")
                 })
-                this.count = data.length
                 this.images = data
+                this.row_count4 = data.length / 4
+                this.row_count3 = data.length / 3
+                this.row_count2 = data.length / 2
             })
-        },
-        mesmaths(){
-            if (window.innerWidth >= 992)
-                mesmaths2(4)
-            else if(window.innerWidth >= 768)
-                mesmaths2(3)
-            else if(window.innerWidth >= 576)
-                mesmaths2(2)
-            else if(window.innerWidth < 576)
-                this.row_count = this.images.length
-        },
-        mesmaths2(col_count){
-            var logos = count % col_count 
-            this.row_count = count / col_count
-            while (logos > 0) {
-                this.images.push({id: 50, link: 'link/link', name: 'Moon Rise'})
-                logos--;
-            }
         }
-       
     }
 }
 
