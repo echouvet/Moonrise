0c78277cacef84d78bcd4927e743389267c8f79f
david <david@imac-de-david.home>
2018-12-16 19:21:02 +0100

    added pb-1 to grid looks better

--- client/components/grid/ImageGrid.vue
+++ client/components/grid/ImageGrid.vue
@@ -1,46 +1,46 @@
 <template>
-
+<div>
 <!-- FOUR ROWS -->
-<!--     <div class="flex flex-wrap px-2"> 
+    <div class="lg:flex flex-wrap px-2 hidden "> 
          <div class="w-full sm:w-1/4 px-1">
-            <img v-for="(el, i) in images" v-if="i < row_count4" :src="el.img1" :key="el.id" class="w-full"> 
+            <img v-for="(el, i) in images" v-if="i < row_count4" :src="el.img1" :key="el.id" class="w-full pb-1"> 
         </div>
         <div class="w-full sm:w-1/4 px-1">
-            <img v-for="(el, i) in images" v-if="i >= row_count4 && i < (row_count4 * 2)" :src="el.img1" :key="el.id" class="w-full"> 
+            <img v-for="(el, i) in images" v-if="i >= row_count4 && i < (row_count4 * 2)" :src="el.img1" :key="el.id" class="w-full pb-1"> 
         </div>
         <div class="w-full sm:w-1/4 px-1">
-           <img v-for="(el, i) in images" v-if="i >= (row_count4 * 2) && i < (row_count4 * 3)" :src="el.img1" :key="el.id" class="w-full"> 
+           <img v-for="(el, i) in images" v-if="i >= (row_count4 * 2) && i < (row_count4 * 3)" :src="el.img1" :key="el.id" class="w-full pb-1"> 
         </div>
         <div class="w-full sm:w-1/4 px-1">
-           <img v-for="(el, i) in images" v-if="i >= (row_count4 * 3)" :src="el.img1" :key="el.id" class="w-full"> 
+           <img v-for="(el, i) in images" v-if="i >= (row_count4 * 3)" :src="el.img1" :key="el.id" class="w-full pb-1"> 
         </div>
-    </div> -->
+    </div>
 
 <!-- THREE ROWS -->
-    <div class="flex flex-wrap px-2"> 
+    <div class="hidden md:flex md:flex-wrap px-2 lg:hidden"> 
          <div class="w-full sm:w-1/3 px-1">
-            <img v-for="(el, i) in images" v-if="i < row_count3" :src="el.img1" :key="el.id" class="w-full"> 
+            <img v-for="(el, i) in images" v-if="i < row_count3" :src="el.img1" :key="el.id" class="w-full pb-1"> 
         </div>
         <div class="w-full sm:w-1/3 px-1">
-            <img v-for="(el, i) in images" v-if="i >= row_count3 && i < (row_count3 * 2)" :src="el.img1" :key="el.id" class="w-full"> 
+            <img v-for="(el, i) in images" v-if="i >= row_count3 && i < (row_count3 * 2)" :src="el.img1" :key="el.id" class="w-full pb-1"> 
         </div>
         <div class="w-full sm:w-1/3 px-1">
-           <img v-for="(el, i) in images" v-if="i >= (row_count3 * 2)" :src="el.img1" :key="el.id" class="w-full"> 
+           <img v-for="(el, i) in images" v-if="i >= (row_count3 * 2)" :src="el.img1" :key="el.id" class="w-full pb-1"> 
         </div>
     </div>
 
 <!-- TWO ROWS -->
-<!--     <div class="flex flex-wrap px-2"> 
+    <div class="flex flex-wrap px-2 md:hidden"> 
          <div class="w-full sm:w-1/2 px-1">
-            <img v-for="(el, i) in images" v-if="i < row_count2" :src="el.img1" :key="el.id" class="w-full"> 
+            <img v-for="(el, i) in images" v-if="i < row_count2" :src="el.img1" :key="el.id" class="w-full pb-1"> 
         </div>
         <div class="w-full sm:w-1/2 px-1">
-            <img v-for="(el, i) in images" v-if="i >= row_count2" :src="el.img1" :key="el.id" class="w-full"> 
+            <img v-for="(el, i) in images" v-if="i >= row_count2" :src="el.img1" :key="el.id" class="w-full pb-1"> 
         </div>
     </div>
- -->
 
 
+</div>
 </template>
 
 <script>
@@ -68,7 +68,6 @@ export default {
         {
             this.$http.get('http://localhost:4000/artists').then(response => {
                 var data =  JSON.parse(response.data)
-                console.log(data[0].img1)
                 data.forEach(el => {
                     el.img1 = el.img1.replace("/__nuxt/assets/img/", "/img/")
                 })
