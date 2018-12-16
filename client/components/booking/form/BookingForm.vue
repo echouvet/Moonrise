e88c4ad602cea0de38d892e609d3b5fc2150b552
david <david@imac-de-david.home>
2018-12-16 16:01:41 +0100

    still working on booking request form

--- client/components/booking/form/BookingForm.vue
+++ client/components/booking/form/BookingForm.vue
@@ -45,35 +45,6 @@
         </app-input>
     </div>
   </div>
-  <!-- <div class="flex flex-wrap -mx-3 mb-2">
-    <div class="w-full md:w-1/3 px-3 mb-6 md:mb-0">
-      <label class="block uppercase tracking-wide text-grey-darker text-xs font-bold mb-2" for="grid-city">
-        City
-      </label>
-      <input class="appearance-none block w-full bg-grey-lighter text-grey-darker border border-grey-lighter rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-grey" id="grid-city" type="text" placeholder="Albuquerque">
-    </div>
-    <div class="w-full md:w-1/3 px-3 mb-6 md:mb-0">
-      <label class="block uppercase tracking-wide text-grey-darker text-xs font-bold mb-2" for="grid-state">
-        State
-      </label>
-      <div class="relative">
-        <select class="block appearance-none w-full bg-grey-lighter border border-grey-lighter text-grey-darker py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-grey" id="grid-state">
-          <option>New Mexico</option>
-          <option>Missouri</option>
-          <option>Texas</option>
-        </select>
-        <div class="pointer-events-none absolute pin-y pin-r flex items-center px-2 text-grey-darker">
-          <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"/></svg>
-        </div>
-      </div>
-    </div>
-    <div class="w-full md:w-1/3 px-3 mb-6 md:mb-0">
-      <label class="block uppercase tracking-wide text-grey-darker text-xs font-bold mb-2" for="grid-zip">
-        Zip
-      </label>
-      <input class="appearance-none block w-full bg-grey-lighter text-grey-darker border border-grey-lighter rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-grey" id="grid-zip" type="text" placeholder="90210">
-    </div>
-  </div> -->
 </form>
 </template>
 
@@ -84,8 +55,15 @@
 // Person responsible for signing contract and making payments (full name):	
 
 
-// Company Data for contract and invoice  Company Name: Company/Promoter address (please include zip code): VAT number: 	 Website: Contract signatory full name:
-// Position in Organisation Email address: Office telephone inc full international dial code:			
+// Company Data for contract and invoice 
+// Company Name:
+//Company/Promoter address (please include zip code):
+//VAT number: 	
+//Website:
+//Contract signatory full name:
+// Position in Organisation
+//Email address:
+//Office telephone inc full international dial code:			
 // Mobile telephone inc full international dial code:
 
 // Event date: (DD/MM/YY)
