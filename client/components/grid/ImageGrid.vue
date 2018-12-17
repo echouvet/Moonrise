94b0b0d1c34beb6d623a3bd24bb59023c10082b2
david <david@imac-de-david.home>
2018-12-17 16:35:05 +0100

    updated the grid to link to the artist page

--- client/components/grid/ImageGrid.vue
+++ client/components/grid/ImageGrid.vue
@@ -2,40 +2,121 @@
 <div>
 <!-- FOUR ROWS -->
     <div class="lg:flex lg:flex-wrap lg:px-2 hidden"> 
-         <div class="w-full sm:w-1/4 px-1">
-            <img v-for="(el, i) in getArtists" v-if="i < getGrid.row_count4" :src="el.img1" :key="el.id" class="w-full pb-1"> 
+         <div class="w-full sm:w-1/4 pr-1">
+            <div class="grid pb-1" v-for="(el, i) in getArtists" v-if="i < getGrid.row_count4" :key="el.id">
+                <img :src="el.img1"  class="image w-full  pb-1">
+                <div class="artist-overlay">
+                    <div class="p-4 bg-moonrise">
+                        <nuxt-link :to="{ name: 'artist-slug', params: { slug: el.slug }}" class="whitespace-no-wrap no-underline text-white capitalize">
+                            {{ el.name }}
+                        </nuxt-link>
+                    </div>
+                </div>
+            </div>
         </div>
         <div class="w-full sm:w-1/4 px-1">
-            <img v-for="(el, i) in getArtists" v-if="i >= getGrid.row_count4 && i < (getGrid.row_count4 * 2)" :src="el.img1" :key="el.id" class="w-full pb-1"> 
+            <div class="grid  pb-1" v-for="(el, i) in getArtists" v-if="i >= getGrid.row_count4 && i < (getGrid.row_count4 * 2)" :key="el.id">
+                <img :src="el.img1"  class="image w-full pb-1">
+                <div class="artist-overlay">
+                    <div class="p-4 bg-moonrise">
+                        <nuxt-link :to="{ name: 'artist-slug', params: { slug: el.slug }}" class="whitespace-no-wrap no-underline text-white capitalize">
+                            {{ el.name }}
+                        </nuxt-link>
+                    </div>
+                </div>
+            </div>
         </div>
         <div class="w-full sm:w-1/4 px-1">
-           <img v-for="(el, i) in getArtists" v-if="i >= (getGrid.row_count4 * 2) && i < (getGrid.row_count4 * 3)" :src="el.img1" :key="el.id" class="w-full pb-1"> 
+            <div class="grid  pb-1" v-for="(el, i) in getArtists" v-if="i >= (getGrid.row_count4 * 2) && i < (getGrid.row_count4 * 3)" :src="el.img1" :key="el.id">
+                <img :src="el.img1"  class="image w-full pb-1">
+                <div class="artist-overlay">
+                    <div class="p-4 bg-moonrise">
+                        <nuxt-link :to="{ name: 'artist-slug', params: { slug: el.slug }}" class="whitespace-no-wrap no-underline text-white capitalize">
+                            {{ el.name }}
+                        </nuxt-link>
+                    </div>
+                </div>
+            </div>
         </div>
         <div class="w-full sm:w-1/4 px-1">
-           <img v-for="(el, i) in getArtists" v-if="i >= (getGrid.row_count4 * 3)" :src="el.img1" :key="el.id" class="w-full pb-1"> 
+            <div class="grid  pb-1" v-for="(el, i) in getArtists" v-if="i >= (getGrid.row_count4 * 3)" :src="el.img1" :key="el.id">
+                <img :src="el.img1"  class="image w-full pb-1">
+                <div class="artist-overlay">
+                    <div class="p-4 bg-moonrise">
+                        <nuxt-link :to="{ name: 'artist-slug', params: { slug: el.slug }}" class="whitespace-no-wrap no-underline text-white capitalize">
+                            {{ el.name }}
+                        </nuxt-link>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 
 <!-- THREE ROWS -->
     <div class="hidden md:flex md:flex-wrap md:px-2 lg:hidden"> 
          <div class="w-full sm:w-1/3 px-1">
-            <img v-for="(el, i) in getArtists" v-if="i < getGrid.row_count3" :src="el.img1" :key="el.id" class="w-full pb-1"> 
+             <div class="grid  pb-1" v-for="(el, i) in getArtists" v-if="i < getGrid.row_count3" :src="el.img1" :key="el.id">
+                <img :src="el.img1"  class="image w-full pb-1">
+                <div class="artist-overlay">
+                    <div class="p-4 bg-moonrise">
+                        <nuxt-link :to="{ name: 'artist-slug', params: { slug: el.slug }}" class="whitespace-no-wrap no-underline text-white capitalize">
+                            {{ el.name }}
+                        </nuxt-link>
+                    </div>
+                </div>
+            </div>
         </div>
         <div class="w-full sm:w-1/3 px-1">
-            <img v-for="(el, i) in getArtists" v-if="i >= getGrid.row_count3 && i < (getGrid.row_count3 * 2)" :src="el.img1" :key="el.id" class="w-full pb-1"> 
+            <div class="grid  pb-1" v-for="(el, i) in getArtists" v-if="i >= getGrid.row_count3 && i < (getGrid.row_count3 * 2)" :src="el.img1" :key="el.id">
+                <img :src="el.img1"  class="image w-full pb-1">
+                <div class="artist-overlay">
+                    <div class="p-4 bg-moonrise">
+                        <nuxt-link :to="{ name: 'artist-slug', params: { slug: el.slug }}" class="whitespace-no-wrap no-underline text-white capitalize">
+                            {{ el.name }}
+                        </nuxt-link>
+                    </div>
+                </div>
+            </div>
         </div>
         <div class="w-full sm:w-1/3 px-1">
-           <img v-for="(el, i) in getArtists" v-if="i >= (getGrid.row_count3 * 2)" :src="el.img1" :key="el.id" class="w-full pb-1"> 
+             <div class="grid  pb-1" v-for="(el, i) in getArtists" v-if="i >= (getGrid.row_count3 * 2)" :src="el.img1" :key="el.id" >
+                <img :src="el.img1"  class="image w-full pb-1">
+                <div class="artist-overlay">
+                    <div class="p-4 bg-moonrise">
+                        <nuxt-link :to="{ name: 'artist-slug', params: { slug: el.slug }}" class="whitespace-no-wrap no-underline text-white capitalize">
+                            {{ el.name }}
+                        </nuxt-link>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 
 <!-- TWO ROWS -->
     <div class="flex flex-wrap px-2 md:hidden"> 
          <div class="w-full sm:w-1/2 px-1">
-            <img v-for="(el, i) in getArtists" v-if="i < getGrid.row_count2" :src="el.img1" :key="el.id" class="w-full pb-1"> 
+             <div class="grid  pb-1" v-for="(el, i) in getArtists" v-if="i < getGrid.row_count2" :src="el.img1" :key="el.id">
+                <img :src="el.img1"  class="image w-full pb-1">
+                <div class="artist-overlay">
+                    <div class="p-4 bg-moonrise">
+                        <nuxt-link :to="{ name: 'artist-slug', params: { slug: el.slug }}" class="whitespace-no-wrap no-underline text-white capitalize">
+                            {{ el.name }}
+                        </nuxt-link>
+                    </div>
+                </div>
+            </div>
         </div>
         <div class="w-full sm:w-1/2 px-1">
-            <img v-for="(el, i) in getArtists" v-if="i >= getGrid.row_count2" :src="el.img1" :key="el.id" class="w-full pb-1"> 
+             <div class="grid  pb-1" v-for="(el, i) in getArtists" v-if="i >= getGrid.row_count2" :src="el.img1" :key="el.id">
+                <img :src="el.img1"  class="image w-full pb-1">
+                <div class="artist-overlay">
+                    <div class="p-4 bg-moonrise">
+                        <nuxt-link :to="{ name: 'artist-slug', params: { slug: el.slug }}" class="whitespace-no-wrap no-underline text-white capitalize">
+                            {{ el.name }}
+                        </nuxt-link>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 
@@ -58,6 +139,33 @@ export default {
 </script>
 
 <style>
- 
+.grid {
+  position: relative;
+  width: 100%;
+}
 
+.image {
+  opacity: 1;
+  display: block;
+  transition: .5s ease;
+  backface-visibility: hidden;
+}
+
+.artist-overlay {
+  transition: .5s ease;
+  opacity: 0;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  -ms-transform: translate(-50%, -50%);
+}
+
+.grid:hover .image {
+  opacity: 0.3;
+}
+
+.grid:hover .artist-overlay {
+  opacity: 1;
+}
 </style>
