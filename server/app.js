c987f95a0497fa65af376b9440104a605012fe8f
david <david@imac-de-david.home>
2018-12-20 13:50:29 +0100

    need to redo app.js

--- server/app.js
+++ server/app.js
@@ -2,6 +2,7 @@ const 	express = require('express')
 const	http = require("http")
 const	mysql = require('mysql')
 const	fs = require('fs')
+const cookieParser = require('cookie-parser')
 const   bodyParser = require('body-parser')
 const   formidable = require('formidable')
 const 	cors = require('cors')
@@ -14,7 +15,7 @@ const   MemoryStore = require('session-memory-store')(ssn)
 const 	bcrypt = require('bcrypt') 
 const	app = express()
 const   server = http.createServer(app)
-const cookieParser = require('cookie-parser')
+
 
 
 const sessionMiddleware = ssn({ 
@@ -24,11 +25,11 @@ const sessionMiddleware = ssn({
     resave: true, 
     saveUninitialized: true
 });
+app.use(cookieParser())
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cors())
+app.use(cors('*'))
 app.use(sessionMiddleware);
-app.use(cookieParser())
 
 var con = mysql.createConnection({
     host: "localhost",
@@ -38,6 +39,7 @@ var con = mysql.createConnection({
 con.connect((err) => { if (err) tools.error(err)
     eval(fs.readFileSync(__dirname + "/database.js")+'')
 })
+
 var secret = Buffer.from('something weird that nobody will guess', 'hex');
 // PUO CREE UN CMPTE
 	// var pass = "admin"
@@ -51,10 +53,10 @@ var secret = Buffer.from('something weird that nobody will guess', 'hex');
 // Ports
 server.listen(5050)
 app.use((req, res, next) =>{
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-    res.setHeader('Access-Control-Allow-Credentials', true);
+    // res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+    // res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    // res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    // res.setHeader('Access-Control-Allow-Credentials', true);
 	next();
 })
 .get('/artists', (req,res) => {
@@ -94,7 +96,7 @@ app.use((req, res, next) =>{
 	{
 		username = eschtml(req.body.username)
 		password = eschtml(req.body.password)
-		con.query('SELECT * FROM users', (err, users) => { if (err) tools.error(err);
+		con.query('SELECT * FROM user', (err, users) => { if (err) tools.error(err);
 			if (users.length === 0)
 			{
 				 tools.error("NO USERS IN DB MESSAGE ELOI")
@@ -109,7 +111,7 @@ app.use((req, res, next) =>{
 				{
 					bcrypt.compare(password, user.password, (err, result) => {
 						if (result) {
-							var token = jwt.encode({eloi : 'nicolas'}, secret);
+							var accessToken = jwt.encode({eloi : 'nicolas'}, secret);
 							res.json({token: {token} })
 						} else {
 							res.json({error: 'Wrong Password'}) // change for the hackers
@@ -122,18 +124,19 @@ app.use((req, res, next) =>{
 	else
 		res.json({error: 'Empty Field'})
 })
+.use((req, res, next) =>{
+	console.log(req.headers.authorization)
+	// console.log(req.headers.cookie)
+	var decoded = jwt.decode({eloi : 'nicolas'}, secret);
+	if (decoded.eloi === 'nicolas')
+		next();
+	else
+		res.json({error: '404 not found'})
+})
 .get('/user', (req, res) => {
 	res.json({data: "Nicolas"})
 })
-// .use((req, res, next) =>{
-// 	// console.log(req.headers.authorization)
-// 	// console.log(req.headers.cookie)
-// 	var decoded = jwt.decode("Le Token", secret);
-// 	if (decoded.eloi === 'nicolas')
-// 		next();
-// 	else
-// 		res.json({error: '404 not found'})
-// })
+
 .post('/artist/delete', (req,res) => {
 	eval(fs.readFileSync(__dirname + "/delete_artist.js")+'')
 })
